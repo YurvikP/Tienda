f32a21fc09e35176ea3f82539230ec3b475fd5ad
Yurvik Pérez Sánchez <Joselperez630@gmail.com>
2023-04-17 18:06:15 -0600

    Version del proyecto tienda 5

---
+++ src/main/java/com/tienda/service/ClienteService.java
@@ -0,0 +1,22 @@
+package com.tienda.service;
+
+import com.tienda.domain.Cliente;
+import java.util.List;
+
+public interface ClienteService {
+    
+    // Se obtiene un listado de clientes en un List
+    public List<Cliente> getClientes();
+    
+    // Se obtiene un Cliente, a partir del id de un cliente
+    public Cliente getCliente(Cliente cliente);
+    
+    // Se inserta un nuevo cliente si el id del cliente esta vacío
+    // Se actualiza un cliente si el id del cliente NO esta vacío
+    public void save(Cliente cliente);
+    
+    // Se elimina el cliente que tiene el id pasado por parámetro
+    public void delete(Cliente cliente);
+    
+}
+
